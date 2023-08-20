3957e72d7c9de9dbf746d2cd51aebf9908db1a80
unknown <vlad.fominov11@gmail.com>
2023-08-20 15:36:10 +0300

    completed todo task on vue3

---
+++ src/components/AppTodoList.vue
@@ -0,0 +1,42 @@
+<template>
+  <ul class="todo-list">
+    <AddTodoItem
+      v-for="todo in todos"
+      :key="todo.id"
+      :todo="todo"
+      @toggle-todo="toggleTodo"
+      @removeTodo="removeTodo"
+    />
+  </ul>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import AddTodoItem from "./AddTodoItem.vue";
+import { Todo } from "@/types/Todo";
+
+
+
+export default defineComponent({
+  components: {
+    AddTodoItem,
+  },
+  props:{
+    todos:{
+      type: Array as PropType<Todo[]>
+    }
+  },
+  methods: {
+   toggleTodo(id: number) {
+     this.$emit("toggleTodo", id)
+    },
+    removeTodo(id: number){
+      this.$emit("removeTodo", id)
+    }
+  },
+   emits: {
+    toggleTodo: (id: number) => Number.isInteger(id),
+    removeTodo: (id: number) => Number.isInteger(id),
+  },
+});
+</script>
